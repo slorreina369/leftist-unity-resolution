00d0039af1ef18359a9dcf9050242a5c785dd0c9
Lorreina <slorreina369@gmail.com>
2025-01-07 11:48:35 -0800

    set base for website deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+    base: '/',
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, 'views/partials'),
